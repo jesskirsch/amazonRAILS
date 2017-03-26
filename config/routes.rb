f54c048cbe0e655aaddd8b9270f776d193ff6328
Jessica Kirsch <jessicaruthkirsch@gmail.com>
2017-03-26 15:01:09 -0400

    login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   resources :reviews
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
 end
